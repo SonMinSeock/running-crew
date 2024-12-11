412fdc117465b107d65c5c8f78f1f24d91753664
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-12-11 17:49:25 +0900

    작성한 게시글 수정기능 구현 완료

--- src/pages/PostUpdate/index.tsx
+++ src/pages/PostUpdate/index.tsx
@@ -3,32 +3,201 @@ import {
   AttatchFileInput,
   AttatchFileSection,
   Form,
+  PreviewBox,
+  PreviewContainer,
+  PreviewImage,
+  PreviewText,
+  RemoveButton,
   Span,
   SubmitBtn,
   Textarea,
   TitleInput,
 } from "../../components/Form";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
+import { FormEvent, useState } from "react";
+import { updateDoc, doc } from "firebase/firestore";
+import { db, storage } from "../../firebase";
+import { deleteObject, getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { useNavigate } from "react-router-dom";
 
 function PostUpdate() {
+  const user = useSelector((state: RootState) => state.userSlice);
+  const { post } = useSelector((state: RootState) => state.postSlice);
+  const [isDragActive, setIsDragActive] = useState(false);
+  const [file, setFile] = useState<File | null>(null);
+  const [title, setTitle] = useState(post?.title);
+  const [description, setDescription] = useState(post?.description);
+  const [imgUrl, setImgUrl] = useState<string | null>(post?.imgUrl ?? null);
+  const navigate = useNavigate();
+
   const handleClickFileInput = () => {
     const fileInput = document.getElementById("file") as HTMLInputElement;
     if (fileInput) {
       fileInput.click(); // 파일 입력 창 열기
     }
   };
+
+  const handleDragStart = () => setIsDragActive(true);
+  const handleDragEnd = () => setIsDragActive(false);
+  const handleDragOver = (event: React.DragEvent) => {
+    event.preventDefault();
+  };
+
+  const handleDrop = (event: React.DragEvent) => {
+    event.preventDefault(); // 기본 이벤트 방지
+    setIsDragActive(false); // 드래그 상태 비활성화
+
+    const files = Array.from(event.dataTransfer.files); // 드롭된 파일 가져오기
+    if (files.length === 1) {
+      setFile(files[0]); // 단일 파일만 추가
+      setImgUrl(null);
+    } else {
+      alert(`최대 1개의 파일만 업로드 가능합니다.`);
+    }
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const files = Array.from(event.target.files);
+      if (files.length === 1) {
+        setFile(files[0]); // 단일 파일만 추가
+        setImgUrl(null);
+      } else {
+        alert(`최대 1개의 파일만 업로드 가능합니다.`);
+      }
+    }
+  };
+
+  const handleRemoveFile = (file?: File) => {
+    if (file) {
+      setFile(null);
+    } else {
+      setImgUrl(null);
+    }
+  };
+
+  const onValid = () => {
+    if (title?.trim().length === 0 && description?.trim().length === 0) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const isValid = onValid();
+
+    if (!isValid) {
+      alert("제목 혹은 모집글 작성해야 합니다.");
+      return;
+    }
+
+    try {
+      const docRef = doc(db, "posts", post?.id ?? "");
+      if (title !== post?.title) {
+        await updateDoc(docRef, {
+          title,
+        });
+      }
+      if (description !== post?.description) {
+        await updateDoc(docRef, {
+          description,
+        });
+      }
+
+      if (title !== post?.title || description !== post?.description) {
+        await updateDoc(docRef, {
+          title,
+          description,
+        });
+      }
+
+      if (file) {
+        const locationRef = ref(storage, `posts/${user.userId}/${post?.id}`);
+        if (post?.imgUrl) {
+          await deleteObject(locationRef);
+        }
+
+        const result = await uploadBytes(locationRef, file);
+        const url = await getDownloadURL(result.ref);
+        await updateDoc(docRef, {
+          imgUrl: url,
+        });
+      } else {
+        const locationRef = ref(storage, `posts/${user.userId}/${post?.id}`);
+
+        await deleteObject(locationRef);
+        await updateDoc(docRef, {
+          imgUrl: null,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setTitle("");
+    setDescription("");
+    setFile(null);
+
+    alert("게시글 수정 성공했습니다.");
+
+    navigate("/");
+  };
+
+  const onTitleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  const onDescriptionInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setDescription(event.target.value);
+  };
+
   return (
-    <Form>
+    <Form onSubmit={onSubmit}>
       <section>
-        <TitleInput placeholder="러닝 모집 제목 작성해 주세요 수정" />
+        <TitleInput placeholder="러닝 모집 제목 작성해 주세요 수정" value={title} onChange={onTitleInputChange} />
       </section>
       <section>
-        <Textarea rows={12} placeholder="러닝 모집글 작성해 주세요 수정" />
+        <Textarea
+          rows={12}
+          placeholder="러닝 모집글 작성해 주세요 수정"
+          value={description}
+          onChange={onDescriptionInputChange}
+        />
       </section>
-      <AttatchFileSection onClick={handleClickFileInput}>
+      <AttatchFileSection
+        onClick={handleClickFileInput}
+        onDragEnter={handleDragStart}
+        onDragLeave={handleDragEnd}
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
+        className={isDragActive ? "isDragActive" : ""}
+      >
         <CiImageOn />
         <Span>클릭 또는 드래그 이미지 업로드</Span>
-        <AttatchFileInput id="file" type="file" />
+        <AttatchFileInput id="file" type="file" accept="image/*" onChange={handleFileChange} />
       </AttatchFileSection>
+      <PreviewContainer>
+        {file ? (
+          <PreviewBox key={file.name}>
+            {file.type.startsWith("image/") ? (
+              <PreviewImage src={URL.createObjectURL(file)} alt={file.name} />
+            ) : (
+              <PreviewText>{file.name}</PreviewText>
+            )}
+            <RemoveButton onClick={() => handleRemoveFile(file)}>×</RemoveButton>
+          </PreviewBox>
+        ) : (
+          imgUrl && (
+            <PreviewBox key={imgUrl}>
+              {imgUrl ? <PreviewImage src={imgUrl} alt="업로드 할 이미지" /> : <PreviewText>이미지</PreviewText>}
+              <RemoveButton onClick={() => handleRemoveFile()}>×</RemoveButton>
+            </PreviewBox>
+          )
+        )}
+      </PreviewContainer>
+
       <SubmitBtn>수정</SubmitBtn>
     </Form>
   );
